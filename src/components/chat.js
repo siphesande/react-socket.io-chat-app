d9efecc35d224ef35c60f812fe78831280d52a24
Nurha <nurha@projectcodex.co>
2019-07-26 16:00:37 +0200

    added react scroll to bottom

--- src/components/chat.js
+++ src/components/chat.js
@@ -10,75 +10,69 @@ import Divider from '@material-ui/core/Divider';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography'
+import ScrollToBottom from 'react-scroll-to-bottom';
+import { css } from 'glamor';
+
 const socket = io('http://localhost:8000',{ transports: ['websocket'] });
 
 export default class extends Component {
     state = {
-        endpoint: 'http://localhost:8000/',
         name : JSON.parse(localStorage.getItem('name')),
         messages: [],
         newMessage: '',
-        startTime: new Date("1000-00-00T12:00:00Z"),
         difference: '0 seconds ago',
-        timeDifference: []
+        online_users: []
     }
     componentDidMount = () =>  {
         socket.on('chat', message => {
-          let messages = this.state.messages
-            messages.push(message)
-            // this.setMessages(messages,message.startTime)
+            this.setState(prevState => ({
+                messages: [...prevState.messages, message]
+              }));
+       
         })
-
-        this.interval = setInterval(() => this.updateTimeDifference(), 10000);
-      }
-
+        this.interval = setInterval(() => this.updateTimePassAfterMessageHasSent(), 2000);
+        this.getOnlineUsers()
+        
+    }
     componentWillUnmount = ()  => {
         socket.close()
         clearInterval(this.interval)
     }
-    updateTimeDifference =  () => {
-        this.state.messages.forEach((chat_msg)=>{
-            
+    updateTimePassAfterMessageHasSent =  () => {
+        var array_of_messages = []
+        this.state.messages.forEach((chat_message)=> {
             const today = new Date();
-            const endDate = new Date(chat_msg.startTime);
+            const endDate = new Date(chat_message.startTime);
             const days = parseInt((endDate - today) / (1000 * 60 * 60 * 24));
             const hours = parseInt(Math.abs(endDate - today) / (1000 * 60 * 60) % 24);
             const minutes = parseInt(Math.abs(endDate.getTime() - today.getTime()) / (1000 * 60) % 60);
             const seconds = parseInt(Math.abs(endDate.getTime() - today.getTime()) / (1000) % 60);
             
-            var new_messages ={name:chat_msg.name,
-                                message: chat_msg.newMessage,
-                                timestamp: chat_msg.timestamp,
-                                startTime: chat_msg.startTime,
-                             }
-            // if(minutes > 60){
-            //     this.setState({difference:hours})
-                
-            // }else if(hours > 24){
-            //     this.setState({difference:days +  ' ' + 'days ago'})
-            // }else if(minutes <= 0 || minutes === NaN ){
-                chat_msg.timeDifference = seconds;
-
-                
-                console.log('chat_msg', chat_msg)
-                // this.setState({messages:seconds +  ' ' + 'sec ago'})
-                
-            
-            // }else{
-            //     this.setState({difference:minutes +  ' ' + 'min ago'})
-            //     this.setState({timeDifference:minutes})
-            // }
-            
+            if(minutes > 60){
+                chat_message.timeDifference = hours + ' ' + 'hour/s ago'
+                array_of_messages.push(chat_message)
+            }else if(hours > 24){
+                chat_message.timeDifference = days + ' ' + 'day/s ago'
+                array_of_messages.push(chat_message)
+            }else if(minutes <= 0 ){
+                chat_message.timeDifference = seconds + ' ' + 'second/s ago'
+                array_of_messages.push(chat_message)
+            }else{
+                chat_message.timeDifference = minutes + ' ' + 'minute/s ago'
+                array_of_messages.push(chat_message)
+            }
         })
-      
+       this.setState({message:array_of_messages})
+       
     }
-    setMessages = (chat_message) => {
-        // this.setState( { messages: chat_message, startTime: new Date(start_time)})
-        this.setState( { messages: chat_message})
-    }
-
     handleSubmitName = (name) => {
         this.setState({name:name})
+    }
+    getOnlineUsers = () => {
+        var login_users = [];
+        var online_users = login_users.push(this.state.name)
+        this.setState({online_users: online_users})
+        console.log('this.state.online_users', login_users.push(this.state.name))
     }
     handleSubmitMasseges = (e) => {
         e.preventDefault()
@@ -92,15 +86,24 @@ export default class extends Component {
             newMessage:''
         })
     }
+
     render() {
-        const {open, name, messages, newMessage} = this.state;
+        const { name, messages, newMessage} = this.state;
+        const ROOT_CSS = css({
+            height: 300
+          });
+    
          return (
             <div>
-                <List style={{height:'400px'}}>
+                 <ScrollToBottom className={ ROOT_CSS }>
+                 
+                <List 
+                       
+                    > 
+                          
                     {
                         messages.map((message, i) => [
-                           <ListItem alignItems="flex-start" key={i}>
-                        
+                           <ListItem alignItems="flex-start" key={i} >
                                 <ListItemAvatar>
                                     <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                                 </ListItemAvatar>
@@ -115,16 +118,17 @@ export default class extends Component {
                                         >
                                             {message.message}
                                         </Typography>
-                                        {'  '}{this.state.difference}
+                                        {'  '}{message.timeDifference}
                                     </React.Fragment>
-                            
                                     }
                                 />
                              </ListItem>,
                             <Divider variant="inset" component="li" />
                         ]
                     )}
+                    
                 </List>
+                </ScrollToBottom>
                 <div style={{display:flexbox,bottom:0}}>
                     <TextField
                         position="fixed"
